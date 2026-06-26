5d493268c99ad546166d1dc077a2b5aa13f7dae8
Rohith Malgari <rohith.malgari@techdenali.com>
2026-06-26 15:34:32 +0530

    checkbox as a reusable component

---
+++ src/lib/components/atoms/Checkbox/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Checkbox.styles.scss";
+
+import { CheckboxProps } from "./Checkbox.types";
+
+const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  checked,
+  disabled = false,
+  onChange,
+}) => {
+  return (
+    <label className="checkbox">
+      <input
+        type="checkbox"
+        className="checkbox__input"
+        checked={checked}
+        disabled={disabled}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+
+      <span className="checkbox__custom"></span>
+
+      {label && (
+        <span className="checkbox__label">
+          {label}
+        </span>
+      )}
+    </label>
+  );
+};
+
+export default Checkbox;
