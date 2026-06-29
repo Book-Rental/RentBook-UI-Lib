5d8f96f9638de15c43f84aeabaf16b70caadbb52
sunayana-lakki <sunayana.lakki@techdenali.com>
2026-06-29 11:58:46 +0530

    Developed the input and label resuable ui components

--- src/lib/components/atoms/index.ts
+++ src/lib/components/atoms/index.ts
@@ -3,6 +3,7 @@ import Image from "./Image";
 import Checkbox from "./Checkbox";
 import Dropdown from "./Dropdown";
 import PriceRangeSlider from "./PriceRangeSlider";
+import Input from "./Input";
+import Label from "./Label";
 
-
-export default { Button, Image, Checkbox, Dropdown, PriceRangeSlider }
+export default { Button, Image, Checkbox, Dropdown, PriceRangeSlider, Label, Input }
