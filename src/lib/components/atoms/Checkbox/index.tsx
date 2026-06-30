96ade5763ebba39c34d5016010e63a016018989c
Nishant8209 <nishant.prabhu@techdenali.com>
2026-06-30 14:50:42 +0530

    resolved the lint issue in CI CD

--- src/lib/components/atoms/Checkbox/index.tsx
+++ src/lib/components/atoms/Checkbox/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "./Checkbox.styles.scss";
-import Button from "../Button";
-import { CheckboxProps } from "./Checkbox.types";
+import React from 'react';
+import './Checkbox.styles.scss';
+
+import { CheckboxProps } from './Checkbox.types';
 
 const Checkbox: React.FC<CheckboxProps> = ({
   label,
