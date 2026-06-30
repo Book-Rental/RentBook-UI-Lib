55a780b7d9706b110e0205c79b229e16edea17c8
Rohith Malgari <rohith.malgari@techdenali.com>
2026-06-30 17:54:29 +0530

    Fixed Merge conflicts

--- src/lib/components/atoms/PriceRangeSlider/index.tsx
+++ src/lib/components/atoms/PriceRangeSlider/index.tsx
@@ -10,74 +10,74 @@ import Tooltip from "./components/Tooltip";
 import { PriceRangeSliderProps } from './PriceRangeSlider.types';
 
 const PriceRangeSlider = ({
-    min,
-    max,
-    value,
-    step = 1,
-    currency = "₹",
-    showTooltip = true,
-    onChange,
+  min,
+  max,
+  value,
+  step = 1,
+  currency = "₹",
+  showTooltip = true,
+  onChange,
 }: PriceRangeSliderProps) => {
-    const [minValue, maxValue] = value;
+  const [minValue, maxValue] = value;
 
-    const left = valueToPercent(
-        minValue,
-        min,
-        max
-    );
+  const left = valueToPercent(
+    minValue,
+    min,
+    max
+  );
 
-    const right = valueToPercent(
-        maxValue,
-        min,
-        max
-    );
+  const right = valueToPercent(
+    maxValue,
+    min,
+    max
+  );
 
-    const { trackRef, startDragging } =
-        useSlider({
-            min,
-            max,
-            step,
-            value,
-            onChange,
-        });
+  const { trackRef, startDragging } =
+    useSlider({
+      min,
+      max,
+      step,
+      value,
+      onChange,
+    });
 
-    return (
-        <div className="price-slider">
-            <div className="price-slider__container">
+  return (
+    <div className="price-slider">
+      <div className="price-slider__container">
 
-                <Track
-                    left={left}
-                    right={right}
-                    trackRef={trackRef}
-                />
+        <Track
+          left={left}
+          right={right}
+          trackRef={trackRef}
+        />
 
-                {showTooltip && (
-                    <>
-                        <Tooltip
-                            left={left}
-                            value={`${currency}${minValue}`}
-                        />
+        {showTooltip && (
+          <>
+            <Tooltip
+              left={left}
+              value={`${currency}${minValue}`}
+            />
 
-                        <Tooltip
-                            left={right}
-                            value={`${currency}${maxValue}`}
-                        />
-                    </>
-                )}
+            <Tooltip
+              left={right}
+              value={`${currency}${maxValue}`}
+            />
+          </>
+        )}
 
-                <Thumb
-                    left={left}
-                    onPointerDown={startDragging(0)}
-                />
+        <Thumb
+          left={left}
+          onPointerDown={startDragging(0)}
+        />
 
-                <Thumb
-                    left={right}
-                    onPointerDown={startDragging(1)}
-                />
+        <Thumb
+          left={right}
+          onPointerDown={startDragging(1)}
+        />
 
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PriceRangeSlider;
